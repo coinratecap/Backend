616140b7a0cd77e3f0ac6238cd64102de3eab766
dhakalkushal <55182298+dhakalkushal@users.noreply.github.com>
2021-01-06 09:19:39 +0545

    modified models

--- src/modules/models/Coin.js
+++ src/modules/models/Coin.js
@@ -2,31 +2,66 @@ const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 
 const coinSchema = mongoose.Schema({
-    code: {
+    id: {
         type: String,
-        required: [true, 'Code is required'],
+        required: [true, 'Id is required'],
         unique: true,
+    },
+    symbol: {
+        type: String,
+        required: [true, 'Symbol is required'],
     },
     name: {
         type: String,
         required: [true, 'Name is required'],
-        unique: true,
+    },
+    is_active: {
+        type: Boolean,
+        required: [true, 'Status is required'],
     },
     price: {
         type: Number,
-        required: [true, 'Price is required']
+        required: [true, 'Price is required'],
     },
-    oneHr: {
-        type: Number,
-        required: [true, 'Hourly rate is required']
-    },
-    twentyfourHr: {
+    logo: {
         type: String,
-        required: [true, 'Daily rate is required']
+        required: [true, 'Logo is required'],
+    },
+    description: {
+        type: String,
+        required: [true, 'Description is required'],
+    },
+    changeOneHr: {
+        type: Number,
+        required: [true, 'Price Change One Hr is required'],
+    },
+    changeTwentyFourHr: {
+        type: String,
+        required: [true, 'Price Change Twenty Four Hr is required'],
+    },
+    changeSevenDay: {
+        type: String,
+        required: [true, 'Price Change Seven Day is required'],
+    },
+    changeFourteenDay: {
+        type: String,
+        required: [true, 'Price Change Fourteen Day is required'],
+    },
+    changeThirtyDay: {
+        type: String,
+        required: [true, 'Price Change Thirty Day is required'],
+    },
+    changeSixtyDay: {
+        type: String,
+        required: [true, 'Price Change Sixty Day is required'],
+    },
+    changeOneYear: {
+        type: String,
+        required: [true, 'Price Change One Year is required'],
     },
     volume: {
         type: Number,
-        required: [true, 'Volume is required']
+        required: [true, 'Volume is required'],
     },
     marketCap: {
         type: Number,
@@ -36,9 +71,17 @@ const coinSchema = mongoose.Schema({
         type: Number,
         required: [true, 'Market Cap rank is required'],
     },
+    fullyDilutedValuation: {
+        type: Number,
+        required: [true, 'fullyDilutedValuation is required'],
+    },
     maxSupply: {
         type: Number,
         required: [true, 'Max Supply is required'],
+    },
+    circulatingSupply: {
+        type: Number,
+        required: [true, 'Circulating Supply is required'],
     },
     allTimeHigh: {
         type: Number,
@@ -48,17 +91,29 @@ const coinSchema = mongoose.Schema({
         type: Number,
         required: [true, 'All Time Low is required'],
     },
-    circulatingSupply: {
-        type: Number,
-        required: [true, 'Circulating Supply is required'],
-    },
     liquidity: {
         type: Number,
-        required: [true, 'Liquidity value is required']
+        required: [true, 'Liquidity value is required'],
     },
     weeklyGraph: {
         type: Array,
-        required: [true, 'Weekly graph is required']
+        required: [true, 'Weekly graph is required'],
+    },
+    website: {
+        type: String,
+        required: [true, 'Website URL is required'],
+    },
+    technical_doc: {
+        type: String,
+        required: [true, 'Technical Doc URL is required'],
+    },
+    twitter: {
+        type: String,
+        required: [true, 'Twitter URL is required'],
+    },
+    reddit: {
+        type: String,
+        required: [true, 'Reddit URL is required'],
     },
 }, {
     timestamps: true,
--- src/modules/models/Exchanges.js
+++ src/modules/models/Exchanges.js
@@ -2,28 +2,51 @@ const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 
 const exchangeSchema = mongoose.Schema({
+    id: {
+        type: String,
+        required: [true, 'Id is required'],
+        unique: true,
+    },
     name: {
         type: String,
         required: [true, 'Name is required'],
-        unique: true,
-    },
-    website: {
-        type: String,
-        required: [true, 'website is required'],
-        unique: true,
     },
     volume: {
         type: Number,
         required: [true, 'Volume is required']
     },
     trust: {
-        type: Boolean,
+        type: Number,
         required: [true, 'Trust value is required']
     },
-    coin: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Coin'
-    }]
+    trustRank: {
+        type: Number,
+        required: [true, 'Trust Rank is required']
+    },
+    centralized: {
+        type: Boolean,
+        required: [true, 'Centralized field is required'],
+    },
+    image: {
+        type: String,
+        required: [true, 'Image URL is required'],
+    },
+    website: {
+        type: String,
+        required: [true, 'website is required'],
+    },
+    facebook: {
+        type: String,
+        required: [true, 'Technical Doc URL is required'],
+    },
+    twitter: {
+        type: String,
+        required: [true, 'Twitter URL is required'],
+    },
+    reddit: {
+        type: String,
+        required: [true, 'Reddit URL is required'],
+    },
 }, {
     timestamps: true,
 });
