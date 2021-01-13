0f282608078b6086df10e3b6dd8013771ff22ec5
Melody Daniel <melodyogonna@gmail.com>
2021-01-13 10:59:34 +0100

    added express json parser to admin route

--- src/modules/routes/admin.js
+++ src/modules/routes/admin.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const homeCtrl = require("../controllers/admin/home");
 
+router.use(express.json());
 /* GET home page. */
 router.get("/", homeCtrl.home);
 router.post("/addCoinToExchange", homeCtrl.addCoinToExchange);
