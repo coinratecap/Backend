06c3f63acd8ba6a0297e0046a652f44189c68867
Melody Daniel <melodyogonna@gmail.com>
2021-01-13 10:48:52 +0100

    Function to add coins to exchange

--- src/modules/controllers/admin/home.js
+++ src/modules/controllers/admin/home.js
@@ -1,8 +1,28 @@
-class HomeCtrl {
+const express = require("express");
+const { AddCoinToExchange } = require("../../services/admin.services");
 
-    static async home(req, res,next) {
-        res.send('respond with a resource from Coinratecap');
+class HomeCtrl {
+  static async home(req, res, next) {
+    res.send("respond with a resource from Coinratecap");
+  }
+
+  /** Controller to add coin to exchange
+   * @param {express.Request} req - express request object
+   * @param {express.Response} res - express response object
+   * @param {express.NextFunction} next - express next function
+   */
+  static async addCoinToExchange(req, res, next) {
+    const { exchangeId, coinId } = req.body;
+    try {
+      await AddCoinToExchange(exchangeId, coinId);
+      return res.status(200).json({ error: false, message: "coin added" });
+    } catch (e) {
+      console.log(e);
+      return res
+        .status(401)
+        .json({ error: true, message: "could not add coin to exchange" });
     }
+  }
 }
 
 module.exports = HomeCtrl;
--- src/modules/routes/admin.js
+++ src/modules/routes/admin.js
@@ -1,8 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const homeCtrl = require('../controllers/admin/home');
+const homeCtrl = require("../controllers/admin/home");
 
 /* GET home page. */
-router.get('/', homeCtrl.home);
+router.get("/", homeCtrl.home);
+router.post("/addCoinToExchange", homeCtrl.addCoinToExchange);
 
 module.exports = router;
