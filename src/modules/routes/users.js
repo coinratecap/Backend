67f6c2f0f30df57bd769d9897e85533bb2a59430
nateshmbhat <nateshmbhat1@gmail.com>
2021-01-19 05:19:17 +0530

    updated user controller

--- src/modules/routes/users.js
+++ src/modules/routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user.controller');
+const passport = require('passport')
 
 router.use(express.json());
 /* GET home page. */
